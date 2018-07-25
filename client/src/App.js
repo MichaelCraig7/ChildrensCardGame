ab104a227ac6144758b812cc3f7873e184f5b1dc
michaelcraig <m_craig@baylor.edu>
2018-07-25 15:16:04 -0400

    got yugis deck returned

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import axios from 'axios'
 
 
@@ -6,21 +6,26 @@ class App extends Component {
 
   state = {
     cardInfo: {},
-    yugiDeck: {},
+    yugiDeck: [],
     kaibaDeck: {}
   }
 
   componentDidMount() {
-
+    this.getYugisDeck()
   }
 
-  getAllCards = async () => {
+  getYugisDeck = async () => {
     try {
-      let cardsResponse = await axios.get('https://www.ygohub.com/api/all_cards')
-      this.setState({
-        cardInfo: cardsResponse.data
-      })
-    }
+      let res = await axios.get('https://www.ygohub.com/api/set_info?name=Starter Deck: Yugi')
+      let cards = res.data.set.language_cards["English (na)"]
+        cards.map( async (card) => {
+          let cardRes = await axios.get(`https://www.ygohub.com/api/card_info?name=${card.card_name}`)
+          console.log(cardRes)
+            this.setState({
+              yugiDeck: cardRes
+          })
+        })
+      }
     catch (error) {
       console.error(error)
     }
@@ -28,7 +33,7 @@ class App extends Component {
 
   getCard = async (cardName) => {
     try {
-      let cardsResponse = await axios.get(`https://www.ygohub.com/api/card_info?name=${cardName}`)
+      let cardsResponse = await axios.get('https://www.ygohub.com/api/all_cards')
       this.setState({
         cardInfo: cardsResponse.data
       })
@@ -43,8 +48,8 @@ class App extends Component {
       <div>
         <h1>app component</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
