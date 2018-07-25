e36b37154404ef34e22484fdbc0ff8955ba66a42
michaelcraig <m_craig@baylor.edu>
2018-07-25 10:50:54 -0400

    controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    get 'cards/index'
+    get 'cards/show'
+    get 'cards/create'
+    get 'cards/update'
+    get 'cards/destroy'
+  end
+  namespace :api do
+    get 'characters/index'
+    get 'characters/show'
+    get 'characters/create'
+    get 'characters/update'
+    get 'characters/destroy'
+  end
+  namespace :api do
+    get 'users/index'
+    get 'users/show'
+    get 'users/create'
+    get 'users/update'
+    get 'users/destroy'
+  end
+  namespace :api do
+    get 'games/index'
+    get 'games/show'
+    get 'games/create'
+    get 'games/update'
+    get 'games/destroy'
+  end
+  namespace :api do
+    resources :game do
+      resources :user do
+        resources :character do
+          resources :card
+        end
+      end
+    end
+  end
+
 end
