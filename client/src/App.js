f597e85be7f16373e7018d16b4555efc67ce4344
michaelcraig <m_craig@baylor.edu>
2018-07-25 12:09:26 -0400

    axios call for all cards works

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-
-// const getCardInfo = axios.get('https://www.ygohub.com/api/all_cards')
+import axios from 'axios'
 
 
 class App extends Component {
 
+  componentDidMount() {
+    this.getCardInfo()
+  }
+
+  getCardInfo = async () => {
+    try {
+      let cardsResponse = await axios.get('https://www.ygohub.com/api/all_cards')
+      this.setState({
+        cardInfo: cardsResponse.data
+      })
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
 
   render() {
     return (
