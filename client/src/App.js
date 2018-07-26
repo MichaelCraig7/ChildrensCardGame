dd7dc9c1242494326991378bbd382689c5609d0f
michaelcraig <m_craig@baylor.edu>
2018-07-26 10:02:39 -0400

    both characters can go to gameroom

--- client/src/App.js
+++ client/src/App.js
@@ -49,6 +49,7 @@ class App extends Component {
     this.setState(prevState => ({
       yugiSelected: !prevState.yugiSelected
     }))
+    this.toGameRoom()
   }
 
   getKaibasDeck = async () => {
