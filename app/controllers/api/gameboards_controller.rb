02d478dd421271149185cdf72d6dd32d78a4ea4f
michaelcraig <m_craig@baylor.edu>
2018-07-27 16:12:11 -0400

    gameboard controller built out (for now)

--- app/controllers/api/gameboards_controller.rb
+++ app/controllers/api/gameboards_controller.rb
@@ -1,16 +1,27 @@
 class Api::GameboardsController < ApplicationController
   def index
+    @gameboards = CreateJoinTableUsersCard.all
+    render json: @gameboards
   end
 
   def show
+    @gameboard = CreateJoinTableUsersCard.find(params[:id])
+    render json: @gameboard
   end
 
   def create
+    @gameboard = CreateJoinTableUsersCard.create(gameboard_params)
+    render json: @gameboard
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+  def gameboard_params
+    params.require(:gameboard).permit(:user_id, :card_id, :apiCard_id)
   end
 end
