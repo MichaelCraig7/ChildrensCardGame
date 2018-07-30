795c53bdda3e477eb4fd49ba27a300734961792d
michaelcraig <m_craig@baylor.edu>
2018-07-29 20:13:46 -0400

    got my idea for the layout, starting changes to homepage styling

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,8 +4,8 @@ import Challenges from './Challenges'
 import styled from 'styled-components'
 
 const HomepageWrapper = styled.div`
-    display: grid;
-    grid-template: repeat(4, 25vh) / repeat(12, 1fr);
+    background: rgb(216, 216, 216);
+
 `
 
 const CharacterSelect = styled.div`
@@ -13,13 +13,26 @@ const CharacterSelect = styled.div`
 `
 
 const CreateGameAndLoop = styled.div`
-    .createGameBtn {
-        grid-area: 3 / 5 / span 1 / span 4;
+    display: flex;
+    display: grid;
+    grid-template: repeat(2, 48vh) / repeat(8, 1fr);
+    grid-gap: 2vw;
+    div {
+        grid-area: 1 / 3 / span 1 / span 4;
+        display: inline-block;
+        align-self: center;
+        text-align: center;
+    }
+    .createGame button{
         background: indianred;
         border-radius: 5px;
+        font-size: -webkit-xxx-large;
+        box-shadow: 1.5px 3px #888888;
     }
-    div {
-        grid-area: 2 / 4 / span 2 / span 4;
+    .createGame {
+        grid-area: 2 / 4 / span 2 / span 2;
+        align-self: center;
+        text-align: center;
     }
 `
 
@@ -41,15 +54,14 @@ class Homepage extends Component {
                         </CharacterSelect>
                         :
                         <CreateGameAndLoop>
-                            <div>
-                                <h1>Challengers</h1>
+                            <div className='challengeBtns'>
                                 <Challenges
                                     challengeList={this.props.challengeList}
                                     acceptGame={this.props.acceptGame}
                                 />
-                                <button onClick={() => x.challengeChecker()}>Update Challenges</button>
                             </div>
-                            <div>
+                                <button onClick={() => x.challengeChecker()}>Update Challenges</button>
+                            <div className='createGame'>
                                 <button className='createGameBtn' onClick={() => x.createGame()}>Create Game</button>
                             </div>
                         </CreateGameAndLoop>
